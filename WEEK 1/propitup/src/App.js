51dc24a9dc02620ef3a87e9196e91a8cffbf0cfa
triciadrich <36164322+triciadrich@users.noreply.github.com>
2021-11-03 19:00:36 -0700

    putting it together

--- WEEK 1/propitup/src/App.js
+++ WEEK 1/propitup/src/App.js
@@ -1,38 +1,39 @@
 import "./App.css";
 import PersonCard from "./components/PersonCard";
+
 function App() {
   const personcard = [
     {
       firstName: "Tricia",
       lastName: "Richardson",
-      age: "40",
+      age: 40,
       hairColor: "brown",
     },
     {
       firstName: "Brian",
       lastName: "Richardson",
-      age: "40",
+      age: 40,
       hairColor: "bald",
     },
     {
       firstName: "Debbie",
       lastName: "Wager",
-      age: "40",
-      hairColor: "black"
+      age: 30,
+      hairColor: "black",
     },
     {
       firstName: "Billy",
       lastName: "Robinson",
-      age: "30",
-      hairColor: "brown"
-    }
-
+      age: 30,
+      hairColor: "brown",
+    },
   ];
   return (
     <div className="App">
       <p>
-        {personcard.map((element) => (
+        {personcard.map((element, index) => (
           <PersonCard
+            key={index}
             firstName={element.firstName}
             lastName={element.lastName}
             age={element.age}
--- WEEK 1/propitup/src/components/PersonCard.js
+++ WEEK 1/propitup/src/components/PersonCard.js
@@ -1,13 +1,15 @@
-import React from "react";
+import React, { useState } from "react";
 
 const PersonCard = (props) => {
-  const { firstName, lastName, age, hairColor } = props;
+  const { firstName, lastName, hairColor } = props;
+  let [age, setAge] = useState(props.age);
   return (
     <div>
       <p>{firstName}</p>
       <p>{lastName}</p>
       <p>{age}</p>
       <p>{hairColor}</p>
+      <button onClick={(event) => setAge(age + 1)}>Birthday button for {firstName}</button>
       <hr></hr>
     </div>
   );
