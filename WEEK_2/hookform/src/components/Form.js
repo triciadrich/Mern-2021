036519747e6f059b469d8b60c9acec9c8a777596
triciadrich <36164322+triciadrich@users.noreply.github.com>
2021-11-05 22:40:33 -0700

    more forms

--- WEEK_2/hookform/src/components/Form.js
+++ WEEK_2/hookform/src/components/Form.js
@@ -1,10 +1,15 @@
 import React, { useState } from "react";
 const Form = (props) => {
   const [firstName, setFirstName] = useState("");
+  const [firstNameError, setFirstNameError] = useState("");
   const [lastName, setLastName] = useState("");
+  const [lastNameError, setLastNameError] = useState("");
   const [email, setEmail] = useState("");
+  const [emailError, setEmailError] = useState("");
   const [password, setPassword] = useState("");
+  const [passwordError, setPasswordError] = useState("");
   const [confirm, setConfirm] = useState("");
+  const [confirmError, setConfirmError] = useState("");
 
   const createUser = (e) => {
     e.preventDefault();
@@ -15,48 +20,77 @@ const Form = (props) => {
     setPassword("");
     setConfirm("");
   };
+
+  const handleFirstName = (e) => {
+    setFirstName(e.target.value);
+    if (e.target.value.length < 2) {
+      setFirstNameError("Must be 2 characters or more");
+    } else {
+      setFirstNameError("");
+    }
+  };
+  const handleLastName = (e) => {
+    setLastName(e.target.value);
+    if (e.target.value.length < 2) {
+      setLastNameError("Must be 2 characters or more");
+    } else {
+      setLastNameError("");
+    }
+  };
+
+  const handleEmail = (e) => {
+    setEmail(e.target.value);
+    if (e.target.value.length < 5) {
+      setEmailError("Must be 5 characters or more");
+    } else {
+      setEmailError("");
+    }
+  };
+
+  const handlePassword = (e) => {
+    setPassword(e.target.value);
+    if (e.target.value.length < 8) {
+      setPasswordError("Must be 8 characters or more");
+    } else {
+      setPasswordError("");
+    }
+  };
+
+  const handleConfirm = (e) => {
+    setConfirm(e.target.value);
+    if (e.target.value === password) {
+      setConfirmError("");
+    } else {
+      setConfirmError("passwords must match");
+    }
+  };
   return (
     <div>
       <form onSubmit={createUser}>
         <div>
           <label>First name</label>
-          <input
-            type="text"
-            value={firstName}
-            onChange={(e) => setFirstName(e.target.value)}
-          />
+          <input type="text" value={firstName} onChange={handleFirstName} />
+          {firstNameError ? <p>{firstNameError}</p> : ""}
         </div>
         <div>
           <label>Last Name</label>
-          <input
-            type="text"
-            value={lastName}
-            onChange={(e) => setLastName(e.target.value)}
-          />
+          <input type="text" value={lastName} onChange={handleLastName} />
+          {lastNameError ? <p>{lastNameError}</p> : ""}
         </div>
         <div>
           <label>Email</label>
-          <input
-            type="email"
-            value={email}
-            onChange={(e) => setEmail(e.target.value)}
-          />
+          <input type="email" value={email} onChange={handleEmail} />
+          {emailError ? <p>{emailError}</p> : ""}
         </div>
         <div>
           <label>Password</label>
-          <input
-            type="password"
-            value={password}
-            onChange={(e) => setPassword(e.target.value)}
-          />
+          <input type="password" value={password} onChange={handlePassword} />
+          {passwordError ? <p>{passwordError}</p> : ""}
         </div>
         <div>
           <label>Confirm Password</label>
-          <input
-            type="password"
-            value={confirm}
-            onChange={(e) => setConfirm(e.target.value)}
-          />
+          <input type="password" value={confirm} onChange={handleConfirm} />
+          {confirmError ? <p>{confirmError}</p> : ""}
         </div>
       </form>
       <hr></hr>
