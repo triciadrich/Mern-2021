11f58b2f8cdc1d30350629c12c74f2977f9045f4
triciadrich <36164322+triciadrich@users.noreply.github.com>
2021-12-10 19:37:51 -0800

    refactored to be correct

--- week_06/product/client/src/App.js
+++ week_06/product/client/src/App.js
@@ -1,25 +1,17 @@
-import axios from "axios";
-import React, { useEffect, useState } from "react";
+
 import "./App.css";
-import Form from "./components/Form";
 import ProductList from "./components/ProductList";
 import Detail from "./views/Detail";
 import { Router } from '@reach/router'
 function App() {
-  const [products, setProducts] = useState([]);
-  const [loaded, setLoaded] = useState(false);
-  useEffect(() => {
-    axios.get("http://localhost:8000/api/products").then((res) => {
-      setProducts(res.data.products);
-      setLoaded(true);
-    });
-  }, []);
+  
   return (
     <div className="App">
-      <Form />
-      <hr />
-      {loaded && <ProductList products={products} />}
+      
+      
+     
       <Router>
+        <ProductList path ="/products"/>
         <Detail path="/products/:id"/>
       </Router>
     </div>
--- week_06/product/client/src/components/ProductList.js
+++ week_06/product/client/src/components/ProductList.js
@@ -1,14 +1,56 @@
-import React from 'react'
-
-
+import React, { useEffect, useState } from 'react'
+import { Link } from '@reach/router';
+import axios from "axios";
 const ProductList = (props) => {
+  const [products, setProducts] = useState([]);
+  const [loaded, setLoaded] = useState(false);
+  const [title, setTitle] = useState("");
+  const [price, setPrice] = useState("");
+  const [description, setDescripton] = useState("");
+  const onSubmit = e => {
+    e.preventDefault();
+    axios.post('http://localhost:8000/api/product',{
+    title,
+    price,
+    description
+  })
+  .then(res=>console.log(res))
+  .catch(err=>console.log(err))
+  }
+  useEffect(() => {
+    axios.get("http://localhost:8000/api/products").then((res) => {
+      setProducts(res.data.products);
+      setLoaded(true);
+    });
+  }, []);
+  if (!loaded) return (
+    <div>...loading</div>
+  )
     return (
-        <div>
+      <><div>
+        <form onSubmit={onSubmit}>
+          <p>
+            <label>Title</label>
+            <input type='text' onChange={(e) => setTitle(e.target.value)} />
+          </p>
+          <p>
+            <label>Price</label>
+            <input type='text' onChange={(e) => setPrice(e.target.value)} />
+          </p>
+          <p>
+            <label>Description</label>
+            <input type='text' onChange={(e) => setDescripton(e.target.value)} />
+          </p>
+          <p><input type="submit" value="submit" /></p>
+        </form>
+      </div><div>
           <h2>All products</h2>
-            {props.products.map((prod, idx)=>{
-                return <p key={idx}>{prod.title}, {prod.price},{prod.description}</p>
-            })}
-        </div>
+          {products.map((prod, idx) => {
+            return <div key={idx}><Link to={`/products/${prod._id}`}><p>{prod.title}</p>
+            </Link>
+            </div>;
+          })}
+        </div></>
     )
 }
 export default ProductList;
