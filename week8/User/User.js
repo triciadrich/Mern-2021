2653464ec9118ec7e7725c8234fe44d0e17a1322
triciadrich <36164322+triciadrich@users.noreply.github.com>
2021-12-18 18:05:29 -0800

    chaining

--- week8/User/User.js
+++ week8/User/User.js
@@ -1,44 +1,36 @@
 class User {
-  constructor(username, email){
-    this.name = username
-    this.email = email
-    this.accountBal = 0
+  constructor(username, email) {
+    this.name = username;
+    this.email = email;
+    this.accountBal = 0;
   }
 
-   makeDep(amount){
-     this.accountBal += amount
-   }
+  makeDep(amount) {
+    this.accountBal += amount;
+    return this;
+  }
 
-   makeWith(amount){
-     this.accountBal -= amount
-   }
+  makeWith(amount) {
+    this.accountBal -= amount;
+    return this;
+  }
 
-   displayBal(){
-     console.log(this.name, this.accountBal)
-   }
+  displayBal() {
+    console.log(this.name, this.accountBal);
+  }
 
-   transferMoney(otherUser, amount){
-     otherUser.accountBal += amount
-     this.accountBal -= amount
-   }
+  transferMoney(otherUser, amount) {
+    otherUser.accountBal += amount;
+    this.accountBal -= amount;
+  }
 }
 
 tricia = new User("tricia", "triciadrich@gmail.com");
-brian = new User ("Brian", "skinny@knowwhere.net");
-debbie = new User("debbie", "debbie@gmail.com")
-tricia.makeDep(100);
-tricia.makeDep(200);
-tricia.makeDep(300);
-tricia.displayBal();
-brian.makeDep(500);
-brian.makeDep(500);
-brian.makeWith(200);
-brian.makeWith(200);
-brian.displayBal();
-debbie.makeDep(10);
-debbie.makeWith(2);
-debbie.makeWith(2);
-debbie.makeWith(2);
+brian = new User("Brian", "skinny@knowwhere.net");
+debbie = new User("debbie", "debbie@gmail.com");
+tricia.makeDep(100).makeDep(200).makeDep(300).displayBal();
+brian.makeDep(500).makeDep(500).makeWith(200).makeWith(200).displayBal();
+debbie.makeDep(10).makeWith(2).makeWith(2).makeWith(2);
 tricia.transferMoney(brian, 1000);
 brian.displayBal();
 tricia.displayBal();
